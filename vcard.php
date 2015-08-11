444a58de03d2a8b75e54cf7b06e7e2fcb3412d20
knigherrant <knigherrant@gmail.com>
2015-08-11 15:45:30 +0700

    testtttttttttttttttttttttttttttttttt

--- vcard.php
+++ vcard.php
@@ -4,6 +4,7 @@
 * Author.........: Troy Wolf [troy@troywolf.com]
 * Last Modified..: 2005/07/14 13:30:00
 * Description....: A class to generate vCards for contact data.
+ * dasdasdasdsadasdadasdasdasdasdas
 */
 class vcard {
   var $log;
